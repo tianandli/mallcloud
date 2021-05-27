b549a5ec00ac388f50af6d126cf952a0bedbb29b
lijie <949620260@qq.com>
2021-05-27 14:08:12 +0800

    [SM20210527]github上面的配置信息修改了，调用一个刷新接口就可以访问到新的配置信息了

--- config-client/src/main/java/com/lijie/configclient/controller/ConfigClientController.java
+++ config-client/src/main/java/com/lijie/configclient/controller/ConfigClientController.java
@@ -1,6 +1,7 @@
 package com.lijie.configclient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @version: V1.0
  */
 @RestController
+@RefreshScope//用于刷新配置
 public class ConfigClientController {
 
     @Value("${config.info}")
