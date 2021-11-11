4e001d388d03be27e1146c2b442f8316ad1784af
lijie <949620260@qq.com>
2021-11-11 15:45:59 +0800

    [20211111]SpringBootAdmin的学习：创建了一个admin-server和admin-client，这两个可以注册到eureka，通过eureka来监控。  admin-client和admin-server-seurity需要登录密码，也是注册进eureka，然后通过eureka来监控

--- admin-server-security/src/main/java/com/lijie/adminserversecurity/AdminServerSecurityApplication.java
+++ admin-server-security/src/main/java/com/lijie/adminserversecurity/AdminServerSecurityApplication.java
@@ -1,17 +1,18 @@
-package com.lijie.adminserver;
+package com.lijie.adminserversecurity;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableAdminServer//开启AdminServer
-@EnableDiscoveryClient
+
+@EnableDiscoveryClient //被eureka发现
+@EnableAdminServer //@EnableAdminServer
 @SpringBootApplication
-public class AdminServerApplication {
+public class AdminServerSecurityApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AdminServerApplication.class, args);
+        SpringApplication.run(AdminServerSecurityApplication.class, args);
     }
 
 }
